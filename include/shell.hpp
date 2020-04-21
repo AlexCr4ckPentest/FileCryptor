0151ced355998e6097b99ac3ebae5e5afa468a18
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-04-22 00:26:16 +0300

    delete unused field

--- include/shell.hpp
+++ include/shell.hpp
@@ -16,13 +16,10 @@ namespace Cryptor
     {
     private:
         int m_argc; char **m_argv;
-
-        RandomKeyGen random_key_gen;
         FileEncryptorDecryptor file_encryptor_decryptor;
 
         boost::program_options::options_description opt_desc;
         boost::program_options::variables_map opts_map;
-
     public:
         Shell(int argc, char **argv);
         ~Shell();
