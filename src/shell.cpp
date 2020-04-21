dd9012666dc8c4193beeb20555ddc483cd34b052
AlexCr4ckPentest <kerzhakov50@mail.ru>
2020-04-21 16:05:09 +0300

    improved logic of a method run()

--- src/shell.cpp
+++ src/shell.cpp
@@ -3,6 +3,7 @@
 namespace Cryptor
 {
     static constexpr size_t salt_length = 4;
+    static size_t key_length;
 
     Shell::Shell(int argc, char **argv)
         : m_argc {argc}
@@ -13,6 +14,15 @@ namespace Cryptor
             ("help,h", "show help message")
             ("encrypt,e", "encrypt mode")
             ("decrypt,d", "decrypt mode")
+            ("key-length,l", boost::program_options::value<size_t>(&key_length)->default_value(4)
+                                                            ->notifier(
+                                                                [](size_t value)
+                                                                {
+                                                                    if (value > 16) {
+                                                                        std::cerr << Colors::c_red << "[-] " << Colors::c_none << "Key length is large!\n";
+                                                                        exit(1);
+                                                                    }
+                                                                }), "set the key length")
             ("file,f", boost::program_options::value<std::string>(), "set the target file")
         ;
     }
@@ -27,21 +37,25 @@ namespace Cryptor
             boost::program_options::notify(opts_map);
         }
         catch (boost::program_options::invalid_command_line_syntax &err) {
-            std::cerr << Colors::c_red << "[-] " << Colors::c_none << err.what() << "\n";
+            std::cerr << Colors::c_red << "[-] " << Colors::c_none << err.what() << std::endl;
             return 1;
         }
 
         if (opts_map.count("help") || opts_map.size() < 2) {
-            std::cout << opt_desc << "\n";
+            std::cout << opt_desc << std::endl;
             return 0;
         }
 
+        if (opts_map.count("key-length")) {
+            key_length = opts_map["key-length"].as<size_t>();
+        }
+
         if (opts_map.count("encrypt")) {
-            uint64_t encrypt_key = RandomKeyGen::generate_code();
+            uint64_t encrypt_key = RandomKeyGen::generate_code(key_length);
             uint32_t salt = RandomKeyGen::generate_code(salt_length);
 
             file_encryptor_decryptor.set_file_name(opts_map["file"].as<std::string>());
-            return file_encryptor_decryptor.encrypt_file(encrypt_key, salt);
+            file_encryptor_decryptor.encrypt_file(encrypt_key, salt);
         }
         else if (opts_map.count("decrypt")) {
             uint64_t decrypt_key;
@@ -52,7 +66,8 @@ namespace Cryptor
             std::cin >> salt;
 
             file_encryptor_decryptor.set_file_name(opts_map["file"].as<std::string>());
-            return file_encryptor_decryptor.decrypt_file(decrypt_key, salt);
+            file_encryptor_decryptor.decrypt_file(decrypt_key, salt);
         }
+        return 0;
     }
 } // namespace Cryptor
