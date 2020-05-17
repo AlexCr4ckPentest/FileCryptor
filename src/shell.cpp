89275fb0ab7793d0006666f1d64d823aa7c3963f
AlexCr4ckPentest <48525682+AlexCr4ckPentest@users.noreply.github.com>
2020-05-17 13:31:03 +0300

    Update shell.cpp

--- src/shell.cpp
+++ src/shell.cpp
@@ -3,10 +3,11 @@
 namespace Cryptor
 {
     namespace po = boost::program_options;
-    static constexpr size_t salt_length = 4;
-    static size_t key_length;
 
-    Shell::Shell(int argc, char **argv)
+    constexpr static uint8_t salt_length = 4;
+    static uint8_t key_length;
+
+    Shell::Shell(int argc, char **argv) noexcept
         : m_argc {argc}
         , m_argv {argv}
         , opt_desc {"Available options"}
@@ -15,11 +16,11 @@ namespace Cryptor
             ("help,h", "show help message")
             ("encrypt,e", "encrypt mode")
             ("decrypt,d", "decrypt mode")
-            ("key-length,l", po::value<size_t>(&key_length)->default_value(4)
+            ("key-length,l", po::value<uint8_t>(&key_length)->default_value(4)
                         ->notifier([](size_t value)
                         {
-                            if (value > 16) {
-                                std::cerr << Colors::c_red << "[-] " << Colors::c_none << "Key length is large!\n";
+                            if (value <= 1 or value > 16) {
+                                std::cerr << Colors::c_red << "[-] " << Colors::c_none << "Key length is large or small!\n";
                                 std::exit(1);
                             }
                         }), "set the key length")
@@ -36,17 +37,17 @@ namespace Cryptor
         }
     }
 
-    Shell::~Shell()
+    Shell::~Shell() noexcept
     {}
 
-    int Shell::run()
+    int Shell::run() noexcept
     {
         int ret_code {0};
+
         if (opts_map.count("help") || opts_map.size() < 2) {
-            std::cout << opt_desc << "\n";
+            std::cout << opt_desc;
             return ret_code;
         }
-
         if (opts_map.count("encrypt")) {
             uint64_t encrypt_key {RandomKeyGen::generate_code(key_length)};
             uint32_t salt {static_cast<uint32_t>(RandomKeyGen::generate_code(salt_length))};
