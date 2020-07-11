9c8d8dcd69e0deb24a9b98ec40e82011a91471de
AlexCr4ckPentest <48525682+AlexCr4ckPentest@users.noreply.github.com>
2020-07-11 15:27:54 +0300

    Update colors.hpp
    change namespace

--- include/colors.hpp
+++ include/colors.hpp
@@ -1,7 +1,7 @@
 #ifndef _COLORS_HPP_
 #define _COLORS_HPP_
 
-namespace my::colors
+namespace alex::colors
 {
     inline constexpr const char* red      = "\033[1;31m";
     inline constexpr const char* green    = "\033[1;32m";
@@ -10,6 +10,6 @@ namespace my::colors
     inline constexpr const char* cyan     = "\033[1;36m";
     inline constexpr const char* white    = "\033[1;37m";
     inline constexpr const char* none     = "\033[0m";
-} // namespace my::colors
+} // namespace alex::colors
 
 #endif // _COLORS_HPP_
